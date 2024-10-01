1d437d9e4605fad9e725ff823279da6af43f9942
AbdulRehman4481 <abdulrehmanmughal4481@gmail.com>
2024-10-01 18:53:25 +0500

    pages error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@ import Collaboration from "@/(components)/Collaboration/Collaboration";
 import Hero from "@/(components)/Hero/Hero";
 import OpenAi from "@/(components)/OpenAi/OpenAi";
 import Tabs from "@/(components)/Tabs/Tabs";
-import VideoSlider from "@/(components)/VideoSlider/VideoSlider";
 import Work from "@/(components)/Work/Work";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -12,7 +10,6 @@ export default function Home() {
       <Hero />
       <Collaboration />
       <OpenAi />
-      {/* <VideoSlider /> */}
       <Tabs />
       <Work />
     </>
